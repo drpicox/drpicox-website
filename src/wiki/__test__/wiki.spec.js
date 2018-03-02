2fa8fd74215b043ab7d99f485b0551eefc2dddf7
David Rodenas <david.rodenas@gmail.com>
2018-03-02 16:29:12 +0100

    refactor: use spy-middleware package instead of locally define the makeSpyMiddleware

--- src/wiki/__test__/wiki.spec.js
+++ src/wiki/__test__/wiki.spec.js
@@ -1,43 +1,6 @@
+import makeSpyMiddleware from 'spy-middleware'
 import createDucksStore from '../../createDucksStore'
 import { getTopic, fetchTopic, PUT_TOPIC } from '../'
-
-const makeDeferred = () => {
-  let resolve, reject
-  let promise = new Promise((r, e) => {
-    resolve = r
-    reject = e
-  })
-  return { promise, resolve, reject }
-}
-
-const makeSpyMiddleware = () => {
-  const spyMiddleware = () => {
-    const actions = []
-    const untils = {}
-
-    spyMiddleware.actions = []
-    spyMiddleware.until = type => {
-      const action = actions.find(any => any.type === type)
-      if (action) return action
-
-      return this.untilNext(type)
-    }
-    spyMiddleware.untilNext = type => {
-      untils[type] = untils[type] || makeDeferred()
-      return untils[type].promise
-    }
-
-    return next => action => {
-      next(action)
-      actions.push(action)
-
-      const until = untils[action.type]
-      delete untils[action.type]
-      if (until) until.resolve(action)
-    }
-  }
-  return spyMiddleware
-}
 
 describe('wiki', () => {
   let spyMiddleware
